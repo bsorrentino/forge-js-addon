7d0e6b0b39ec545b93fe86e7e28b0973467f6fc2
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-05-30 12:16:18 +0200

    update

--- src/main/java/org/bsc/package-info.java
+++ src/main/java/org/bsc/package-info.java
@@ -1,1 +1,29 @@
+@Java2TS(declare = {
+
+java.lang.String.class,
+java.util.Collection.class,
+java.util.List.class,
+org.jboss.forge.addon.maven.plugins.PluginElement.class,
+org.jboss.forge.addon.maven.plugins.Execution.class,
+org.jboss.forge.addon.dependencies.Dependency.class,
+org.jboss.forge.addon.dependencies.Coordinate.class,
+org.jboss.forge.addon.maven.plugins.ConfigurationElement.class,
+org.jboss.forge.addon.maven.plugins.Configuration.class,
+org.jboss.forge.addon.dependencies.builder.DependencyBuilder.class,
+org.jboss.forge.addon.dependencies.builder.CoordinateBuilder.class,
+org.jboss.forge.addon.maven.plugins.ConfigurationBuilder.class,
+org.jboss.forge.addon.maven.plugins.ConfigurationElementBuilder.class,
+org.jboss.forge.addon.maven.plugins.ExecutionBuilder.class,
+org.jboss.forge.addon.maven.dependencies.MavenDependencyAdapter.class,
+
+org.jboss.forge.addon.ui.input.InputComponentFactory.class,
+org.jboss.forge.addon.ui.context.UIBuilder.class,
+org.jboss.forge.addon.ui.context.UIContextProvider.class,
+org.jboss.forge.addon.ui.context.UIExecutionContext.class,
+org.jboss.forge.addon.ui.result.Result.class,
+org.jboss.forge.addon.ui.result.Results.class,
+org.jboss.forge.addon.ui.wizard.UIWizardStep.class
+
+})
 package org.bsc;
+import org.bsc.processor.annotation.Java2TS;
