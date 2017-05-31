e654fbf978ff7b3fe5acd57790904c07a4f1427e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-05-31 17:25:13 +0200

    add ts declaration

--- src/main/java/org/bsc/package-info.java
+++ src/main/java/org/bsc/package-info.java
@@ -3,6 +3,7 @@
 java.lang.String.class,
 java.util.Collection.class,
 java.util.List.class,
+java.util.Map.class,
 org.jboss.forge.addon.maven.plugins.PluginElement.class,
 org.jboss.forge.addon.maven.plugins.Execution.class,
 org.jboss.forge.addon.dependencies.Dependency.class,
@@ -14,16 +15,21 @@ org.jboss.forge.addon.dependencies.builder.CoordinateBuilder.class,
 org.jboss.forge.addon.maven.plugins.ConfigurationBuilder.class,
 org.jboss.forge.addon.maven.plugins.ConfigurationElementBuilder.class,
 org.jboss.forge.addon.maven.plugins.ExecutionBuilder.class,
+org.jboss.forge.addon.maven.plugins.MavenPluginBuilder.class,
 org.jboss.forge.addon.maven.dependencies.MavenDependencyAdapter.class,
 
+org.jboss.forge.addon.ui.command.UICommand.class,
 org.jboss.forge.addon.ui.input.InputComponentFactory.class,
 org.jboss.forge.addon.ui.context.UIBuilder.class,
 org.jboss.forge.addon.ui.context.UIContextProvider.class,
 org.jboss.forge.addon.ui.context.UIExecutionContext.class,
 org.jboss.forge.addon.ui.result.Result.class,
 org.jboss.forge.addon.ui.result.Results.class,
-org.jboss.forge.addon.ui.wizard.UIWizardStep.class
+org.jboss.forge.addon.ui.wizard.UIWizardStep.class,
+org.jboss.forge.addon.ui.wizard.UIWizard.class,
 
+org.apache.maven.model.Dependency.class,
+org.apache.maven.model.Exclusion.class
 })
 package org.bsc;
 import org.bsc.processor.annotation.Java2TS;
